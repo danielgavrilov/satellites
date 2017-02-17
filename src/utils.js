53deb8edc4dd4bb7fab9d485f60ce177f397a983
Daniel Gavrilov <dgavrilov95@gmail.com>
2017-02-17 17:33:55 +0000

    Add angle conversion util functions

--- src/utils.js
+++ src/utils.js
@@ -10,3 +10,11 @@ export function normalise_angle(angle) {
   if (angle < 0) angle += 2 * π;
   return angle;
 }
+
+export function deg_to_rad(degrees) {
+  return (degrees * π) / 180;
+}
+
+export function rad_to_deg(radians) {
+  return (radians * 180) / π;
+}
