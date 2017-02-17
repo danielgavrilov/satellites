6f34d1d1f71bb49930d229106909a7c3509119e7
Daniel Gavrilov <dgavrilov95@gmail.com>
2017-02-17 23:06:09 +0000

    Add more coordinate transforms

--- public/src/calculations/coordinates.js
+++ public/src/calculations/coordinates.js
@@ -1,8 +1,50 @@
+import _ from "lodash";
+import {norm} from "math";
+
+import {atan2, deg_to_rad} from "./utils";
 import {date_to_j2000_seconds} from "./time";
 
+const R_equatorial = 6378.137;
+const R_polar = 6356.752;
+const R_avg = (R_equatorial + R_polar) / 2;
+
 export function eci_to_ecef(position, date) {
+  if (!_.isDate(date)) {
+    throw new Error("eci_to_ecef() received an invalid time.");
+  }
   const seconds = date_to_j2000_seconds(date);
   const days = seconds / 60 / 60 / 24;
-  const Θ = 280.4606 + 360.9856473662 * days;
+  const Θ = deg_to_rad(280.4606) + deg_to_rad(360.9856473662) * days;
   return rotate_z(position, Θ);
+}
+
+export function ecef_to_topocentric(position) {
+  const [x, y, z] = position;
+  const λ = atan2(y, x);
+  const φ = atan2(z, sqrt(pow(x, 2) + pow(y, 2)));
+  const h = norm(position) - R_avg;
+  return { λ, φ, h };
+}
+
+export function topocentric_to_enu({ λ, φ, h }) {
+
+  const e = [
+    -sin(λ),
+    cos(λ),
+    0
+  ];
+
+  const n = [
+    -cos(λ) * sin(φ),
+    -sin(λ) * sin(φ),
+    cos(φ) * k
+  ];
+
+  const u = [
+    cos(λ) * cos(φ),
+    sin(λ) * cos(φ),
+    sin(φ)
+  ];
+
+  return { e, n, u };
 }
