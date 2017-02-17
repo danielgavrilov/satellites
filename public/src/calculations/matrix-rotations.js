e3ea2332837d28bb49a683019abc15f9ce83986a
Daniel Gavrilov <dgavrilov95@gmail.com>
2017-02-17 19:08:10 +0000

    Add all axis rotation matrices

--- public/src/calculations/matrix-rotations.js
+++ public/src/calculations/matrix-rotations.js
@@ -2,14 +2,42 @@ import {multiply} from "mathjs";
 
 import {sin, cos} from "../utils";
 
-export function rotate_z_matrix(angle) {
-  const sinA = sin(angle);
-  const cosA = cos(angle);
+export function rotate_x_matrix(angle) {
+  const s = sin(angle);
+  const c = cos(angle);
   return [
-    [ cosA,  sinA,  0],
-    [-sinA,  cosA,  0],
-    [     0,    0,  1]
+    [ 1,  0,  0],
+    [ 0,  c,  s],
+    [ 0, -s,  c]
   ];
+};
+
+export function rotate_y_matrix(angle) {
+  const s = sin(angle);
+  const c = cos(angle);
+  return [
+    [ c,  0, -s],
+    [ 0,  1,  0],
+    [ s,  0,  c]
+  ];
+};
+
+export function rotate_z_matrix(angle) {
+  const s = sin(angle);
+  const c = cos(angle);
+  return [
+    [ c,  s,  0],
+    [-s,  c,  0],
+    [ 0,  0,  1]
+  ];
+};
+
+export function rotate_x(M, angle) {
+  return multiply(rotate_x_matrix(angle), M);
+};
+
+export function rotate_y(M, angle) {
+  return multiply(rotate_y_matrix(angle), M);
 };
 
 export function rotate_z(M, angle) {
