49c13e23c9a6036ec9bf65b23241d3bcfe3441ea
Daniel Gavrilov <dgavrilov95@gmail.com>
2017-02-17 17:57:44 +0000

    Simplify solve_kepler

--- public/src/calculations/solve-kepler.js
+++ public/src/calculations/solve-kepler.js
@@ -3,9 +3,10 @@ import {abs, sin, cos} from "../utils";
 export default function solve_kepler(M, e) {
   let E = M;
   let x;
-  while (abs(x = f(M, E, e)) > 1e-7) {
+  do {
+    x = f(M, E, e);
     E -= x / df(E, e);
-  }
+  } while (abs(x) > 1e-7);
   return E;
 }
 
