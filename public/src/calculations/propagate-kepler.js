29358db9ecc04fabdd753e20e5ad5c13db0c5ac2
Daniel Gavrilov <dgavrilov95@gmail.com>
2017-02-17 17:58:45 +0000

    Make propagate_kepler accept dates

--- public/src/calculations/propagate-kepler.js
+++ public/src/calculations/propagate-kepler.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import moment from "moment";
 
 import {sin, cos, sqrt, pow, atan2, norm, J2000} from "../utils";
@@ -5,7 +6,11 @@ import cart2kep from "./cart2kep";
 import {get_gaussian, get_eci_position, get_eci_velocity} from "./kep2cart";
 import solve_kepler from "./solve-kepler";
 
-export default function propagate_kepler({r, v, t, GM}) {
+export default function propagate_kepler({r, v, time, GM}) {
+
+  if (!_.isDate(time)) {
+    throw new Error("propagate_kepler() received an invalid time.");
+  }
 
   const {a, e, i, ω, Ω, ν} = cart2kep({r, v, GM});
   const n = sqrt(GM / pow(a, 3));
@@ -18,9 +23,9 @@ export default function propagate_kepler({r, v, t, GM}) {
   const E0 = atan2(sinE0, cosE0);
   const M0 = E0 - e * sinE0;
 
-  return function(date) {
+  return function(t) {
 
-    const dt = ((+date) - (+t)) / 1000;
+    const dt = ((+t) - (+time)) / 1000;
 
     const Mi = M0 + n * dt;
     const Ei = solve_kepler(Mi, e);
