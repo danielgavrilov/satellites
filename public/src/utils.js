1a5091118301932fe2c7476b1c4e57705a657bbc
Daniel Gavrilov <dgavrilov95@gmail.com>
2017-02-19 19:29:23 +0000

    Whoops, it's 2π not π

--- public/src/utils.js
+++ public/src/utils.js
@@ -2,7 +2,7 @@ export const { PI:π } = Math;
 export const J2000 = new Date(2000, 0, 1, 12, 0, 0);
 
 export function normalise_angle(angle) {
-  angle = angle % π;
+  angle = angle % (2 * π);
   if (angle < 0) angle += 2 * π;
   return angle;
 }
