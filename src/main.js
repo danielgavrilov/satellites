0a6787359e70c8eecb75e4c7e90b95e0bb22c8e1
Daniel Gavrilov <dgavrilov95@gmail.com>
2017-02-19 22:33:52 +0000

    Implement RK4 monopole

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import data from "./data";
 import kep2cart from "./calculations/kep2cart";
 import cart2kep from "./calculations/cart2kep";
 import propagate_kepler from "./calculations/propagate-kepler";
+import propagate_rk4 from "./calculations/propagate-rk4";
 import { eci_to_ecef, ecef_to_latlon } from "./calculations/coordinate-transforms";
 import { rad_to_longitude, rad_to_latitude } from "./utils";
 import draw_map from "./draw/world-map";
@@ -11,38 +12,52 @@ import draw_map from "./draw/world-map";
 const { keplerian, cartesian, GM, time } = data["jason-2"];
 const { r, v } = cartesian;
 
-const world_map = draw_map(d3.select("#world-map"));
+// =============================================================================
 
-const propagate_kepler_jason2 = propagate_kepler({r, v, GM, time});
+function transform_for_plot(cartesian_positions) {
+
+  const latlon_positions = cartesian_positions.map(({ r, time }) => {
+    return {
+      position: ecef_to_latlon(eci_to_ecef(r, time)),
+      time
+    };
+  });
+
+  const coords = latlon_positions.map(({ position }) => {
+    const { λ, φ } = position;
+    return [
+      rad_to_longitude(λ),
+      rad_to_latitude(φ)
+    ];
+  });
+
+  return {
+    type: "LineString",
+    coordinates: coords
+  };
+}
 
 // =============================================================================
 
-const intervals = d3.range(+time, +time + 1 * 8640 * 10e3, 10e3)
+const DAYS = 1;
+
+const world_map_keplerian = draw_map(d3.select("#world-map-keplerian"));
+const world_map_rk4 = draw_map(d3.select("#world-map-rk4"));
+
+
+const propagate_kepler_jason2 = propagate_kepler({r, v, GM, time});
+
+const intervals = d3.range(+time, +time + DAYS * 8640 * 10e3, 10e3)
     .map(ms => new Date(ms));
 
-const cartesian_positions = intervals.map((time) => {
+const predictions_keplerian = intervals.map((time) => {
   const { r, v } = propagate_kepler_jason2(time);
   return { r, v, time };
 });
 
-const latlon_positions = cartesian_positions.map(({ r, time }) => {
-  return {
-    position: ecef_to_latlon(eci_to_ecef(r, time)),
-    time
-  };
-});
 
-const coords = latlon_positions.map(({ position }) => {
-  const { λ, φ } = position;
-  return [
-    rad_to_longitude(λ),
-    rad_to_latitude(φ)
-  ];
-});
+const predictions_rk4 = propagate_rk4({ r, v, GM, time }, DAYS * 8640, 10);
 
-const datum = {
-  type: "LineString",
-  coordinates: coords
-};
 
-world_map.track(datum);
+world_map_keplerian.track(transform_for_plot(predictions_keplerian));
+world_map_rk4.track(transform_for_plot(predictions_rk4));
