c07ad4176882481e8a338e6b14747831a531da24
Daniel Gavrilov <dgavrilov95@gmail.com>
2017-02-22 19:43:15 +0000

    Implement rise and set calculation

---
+++ public/src/rise-and-set.js
@@ -0,0 +1,37 @@
+import station_view from "./transforms/station-view";
+import { group_consecutive } from "./utils/arrays";
+import { deg_to_rad } from "./utils/angles";
+
+export default function rise_and_set(track, station_latlon, mask_angle=5) {
+
+  const mask_angle_rad = deg_to_rad(mask_angle);
+  const get_azimuth_elevation = station_view(station_latlon);
+
+  const items = track.map((point) => {
+    const { elevation, azimuth } = get_azimuth_elevation(point.ecef.r);
+    return {
+      elevation,
+      azimuth,
+      point,
+      time: point.time
+    };
+  });
+
+  const rise_and_set_groups = group_consecutive(items, ({ elevation }) => {
+    return elevation >= mask_angle_rad;
+  });
+
+  const result = rise_and_set_groups.map((items) => {
+    const rise = items[0].time;
+    const set = items[items.length-1].time;
+    const duration = ((+set) - (+rise)) / 1e3;
+    const track = items.map(({ point }) => point);
+    const relative_track = items.map(({ elevation, azimuth, time }) => {
+      return { elevation, azimuth, time };
+    });
+    return { rise, set, duration, track, relative_track };
+  });
+
+  return result;
+
+}
