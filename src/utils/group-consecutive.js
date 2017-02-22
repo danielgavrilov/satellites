20e70a8db4defc18895a6fad0a8268ae13374b02
Daniel Gavrilov <dgavrilov95@gmail.com>
2017-02-22 18:07:28 +0000

    Add group_consecutive

---
+++ src/utils/group-consecutive.js
@@ -0,0 +1,34 @@
+const TAKE = 1,
+      LEAVE = 0;
+
+export default function group_consecutive(array, predicate) {
+
+  let state = LEAVE,
+      groups = [],
+      temp = [];
+
+  array.forEach((x) => {
+
+    const p = predicate(x);
+
+    if (state === LEAVE && p) {
+      state = TAKE;
+    }
+
+    if (state === TAKE) {
+      if (!p) {
+        state = LEAVE;
+        groups.push(temp);
+        temp = [];
+      } else {
+        temp.push(x);
+      }
+    }
+  });
+
+  if (state === TAKE) {
+    groups.push(temp);
+  }
+
+  return groups;
+}
